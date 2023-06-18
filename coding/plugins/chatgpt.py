09b5849c6aced14b0cb6a93a93f58fec7dc4c41c
sunilsaini0 <134995425+sunilsaini0@users.noreply.github.com>
2023-06-18 22:14:38 +0530

    Update chatgpt.py

--- coding/plugins/chatgpt.py
+++ coding/plugins/chatgpt.py
@@ -10,7 +10,7 @@ openai.api_key = "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"
 async def _gpt(event):
   #if event.sender_id ==int(5370621967):
   await event.reply("yes! you are My Developer you developed me very well")
-  # else:
-   # await event.reply("you are user: nice to meet you")
+   else:
+    await event.reply("you are user: nice to meet you")
   
   await event.reply (event)
